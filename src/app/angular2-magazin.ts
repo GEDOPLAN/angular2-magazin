cbd767bd6b5c61713663c3a97f951f1d9e83d458
Dominik Mathmann <dominik.mathmann@gedoplan.de>
2016-02-22 15:44:55 +0100

    - Starter Init

---
+++ src/app/angular2-magazin.ts
@@ -0,0 +1,21 @@
+import {Component} from 'angular2/core';
+import {Router, Route, RouteConfig, ROUTER_DIRECTIVES} from 'angular2/router';
+
+import {Home} from './components/home/home'
+
+
+@Component({
+    selector: 'app',
+    providers: [],
+    templateUrl: 'app/template.html',
+    directives: [ROUTER_DIRECTIVES],
+    pipes: []
+})
+@RouteConfig([
+        new Route({ path: '/home', component: Home, name: 'Home', useAsDefault: true}),
+])
+export class App {
+
+    constructor() { }
+
+}
